723d717b84b46e025ac5ed15f2493aa1d8ee6d75
113991-Bertello <113991@tecnicatura.frc.utn.edu.ar>
2024-09-13 02:10:11 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FileItem } from '../models/file.item.model';
 import { FILE_LIST } from '../data/file.storage';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
